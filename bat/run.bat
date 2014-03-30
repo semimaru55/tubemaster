503e93c8ae6424bfd485699ce38a80867197b7df
semimaru <semimaru55@gmail.com>
2014-03-30 15:52:17 +0900

    Java 1.7

--- bat/run.bat
+++ bat/run.bat
@@ -2,5 +2,5 @@ cd /d %~dp0
 
 rem java.exe -jar tubemaster.jar
 
-"C:\Program Files (x86)\Java\jre6\bin\java.exe" -jar tubemaster.jar
+"C:\Program Files\Java\jre7\bin\java.exe" -jar tubemaster.jar
 
